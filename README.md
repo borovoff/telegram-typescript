693deff3aa4f98dfdc05e4fbdc200bc0dd6006f6
borovoff <borovov.alexandr@gmail.com>
2019-11-17 20:45:54 +0300

    webpack deploy, assets, readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Telegram TypeScript client
+
+The app is based on [TDLib](https://github.com/tdlib/td). Try the client [here](https://borovoff.github.io/telegram-client/).
+
+### Prerequisites (Node.js)
+
+MacOS
+```
+brew install node
+```
+
+Ubuntu
+```
+sudo apt install nodejs
+```
+
+### Deployment
+
+```
+npm install & npm run deploy
+```
+
+Script creates the dist folder. Copy all files from there to your server. 
+
+### Development
+
+```
+npm install & npm run deploy & npm run start
+```
+ 
+The third part runs webpack on localhost:8080
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './js/app.js',
@@ -9,5 +10,14 @@ module.exports = {
     devServer: {
         contentBase: './dist',
     },
-    devtool: "source-map"
+    // devtool: "source-map",
+    plugins: [
+        new CopyPlugin([
+            { from: './src/assets', to: 'assets' },
+            { from: './src/index.html' },
+            { from: '*.worker.js', context: './node_modules/tdweb/dist/'},
+            { from: '*.mem', context: './node_modules/tdweb/dist/'},
+            { from: '*.wasm', context: './node_modules/tdweb/dist/'},
+        ])
+    ]
 };
